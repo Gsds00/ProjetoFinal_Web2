21ec7e774c49ec5679224f437233e0f2311e8a8b
Gabriel Severino <gabrielseverinooo000@gmail.com>
2022-08-18 18:23:29 -0300

    concerto

--- database/dao.js
+++ database/dao.js
@@ -8,11 +8,11 @@ findById(id){
    return pool.promise().query('select * from dadospro where id=?', [id])
 },
 save: function(dadospro){
-  return pool.promise().execute(' INSERT INTO dadospro(nome, quantidade, preco, marca, disconto)VALUES (?,?,?,?,?)', [dadospro.nome, dadospro.quantidade, dadospro.preco, dadospro.marca,dadospro.disconto])
+  return pool.promise().execute(' INSERT INTO dadospro(nomepr, quantidade, preco, marca, nome)VALUES (?,?,?,?,?)', [dadospro.nomepr, dadospro.quantidade, dadospro.preco, dadospro.marca,dadospro.nome])
 },
 update: function(dadospro){
 
-  return pool.promise().execute('UPDATE dadospro set nome=?, quantidade=?, preco=?, marca=?, disconto=? where id=?', [dadospro.nome, dadospro.quantidade, dadospro.preco, dadospro.marca, dadospro.disconto, dadospro.id])
+  return pool.promise().execute('UPDATE dadospro set nomepr=?, quantidade=?, preco=?, marca=?, nome=? where id=?', [dadospro.nomepr, dadospro.quantidade, dadospro.preco, dadospro.marca, dadospro.nome, dadospro.id])
 },
 remove: function(id){
       return pool.promise().execute('delete from dadospro where id= ?' , [id])
@@ -20,6 +20,10 @@ remove: function(id){
 search: function(nome){
   return pool.promise().query('select * from dadospro where nome like ?', ['%'+nome+'%' ])
 },
+
+/*findById(id){
+  return pool.promise().query('select * from dadospro, dadospro where id=?', [id])
+},*/
 
 findByUsername:function(username){
     return pool.promise().query('select * from dadospro where login=?', [username])
